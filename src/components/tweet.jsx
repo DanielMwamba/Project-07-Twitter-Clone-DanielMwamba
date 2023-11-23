8902907a5b2a27284233b240ddbdc1fd05624fa2
DanielMwamba <danmuamba81@gmail.com>
2023-11-23 14:35:12 +0100

    chore: Update files with refinements and enhancements

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -7,8 +7,12 @@ function Tweet({ tweet }) {
         <>
 
             <div className="tweet">
-                <img src={tweet.avatar} alt="" />
+                <div className='tweet-avatar'>
+                    <img src={tweet.avatar} alt="" />
+                </div>
+                
                 <div className='tweet-content'>
+
                     <div className='tweet-body'>
                         <div className='tweet-title'>
                             <p className='tweet-title-author'> {tweet.title} </p>
@@ -18,15 +22,15 @@ function Tweet({ tweet }) {
                         </div>
 
                         <p className='tweet-text'> {tweet.content} </p>
-                        <div className='tweet-image'> <img src={tweet.Image} alt="" /></div>
-                        <div className='tweet-actions'>
+                        <div className='tweet-image'> <img src={tweet.Image} alt="" /></div>                   
+                    </div>
+
+                    <div className='tweet-actions'>
                             <span className='tweet-action'>  <img src={tweet.iconAction.iconReply} /> {tweet.details.actionCountComments} </span>
                             <span className='tweet-action'>  <img src={tweet.iconAction.iconRetweet} /> {tweet.details.actionCountRetweets} </span>
-                            <span className='tweet-action'>  <img src={tweet.iconAction.iconreact} /> {tweet.details.actionCountLikes} </span>
+                            <span className='tweet-action'>  <img src={tweet.iconAction.iconReact} /> {tweet.details.actionCountLikes} </span>
                             <span className='tweet-action'>  <img src={tweet.iconAction.iconShare} /> {tweet.details.actionCountShares} </span>
                         </div>
-
-                    </div>
                 </div>
             </div>
         </>
--- src/components/tweetsData.js
+++ src/components/tweetsData.js
@@ -11,9 +11,9 @@ const tweetsData = [
     imgverified: vector,
     content: 'President Joe Biden touted a new agreement reached whith the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
     details: {
-      actionCountRetweets: 144,
-      actionCountComments: 57,
-      actionCountLikes: 184,
+      actionCountRetweets: "144",
+      actionCountComments: "57",
+      actionCountLikes: "184",
       actionCountShares: null,
 
     },
@@ -36,9 +36,9 @@ const tweetsData = [
     imgverified: vector,
     content: 'Gardening boomed during the pandemic. Six Black writers share how has helped re-establish, and reimage, a connection to cultivation and the land.',
     details: {
-      actionCountRetweets: 48,
-      actionCountComments: 19,
-      actionCountLikes: 482,
+      actionCountRetweets: "48",
+      actionCountComments: "19",
+      actionCountLikes: "482",
       actionCountShares: null,
 
     },
@@ -61,9 +61,9 @@ const tweetsData = [
     imgverified: vector,
     content: 'BIG NEWS lol jk still Twetter.',
     details: {
-      actionCountRetweets: 48,
-      actionCountComments: 19,
-      actionCountLikes: 482,
+      actionCountRetweets: "785.4K",
+      actionCountComments: "118.7K",
+      actionCountLikes: "3.3M",
       actionCountShares: null,
 
     },
@@ -86,9 +86,9 @@ const tweetsData = [
     imgverified: vector,
     content: 'hello litteraly everyone',
     details: {
-      actionCountRetweets: 48,
-      actionCountComments: 19,
-      actionCountLikes: 482,
+      actionCountRetweets: "785.4K",
+      actionCountComments: "118.7K",
+      actionCountLikes: "3.3M",
       actionCountShares: null,
 
     },
@@ -111,9 +111,9 @@ const tweetsData = [
     imgverified: vector,
     content: 'hello litteraly everyone',
     details: {
-      actionCountRetweets: 48,
-      actionCountComments: 19,
-      actionCountLikes: 482,
+      actionCountRetweets: "785.4K",
+      actionCountComments: "118.7K",
+      actionCountLikes: "3.3M",
       actionCountShares: null,
 
     },
